d79f5c12aea5f1808bc67a7373eb0b866cbca83c
Trebolf <wangsawihardja_fvw@yahoo.com>
2022-04-05 13:23:33 -0500

    Added functionalities: Update User Info & View Profile/Posts

--- DownToEarth/src/main/java/com/earth/DownToEarth/models/User.java
+++ DownToEarth/src/main/java/com/earth/DownToEarth/models/User.java
@@ -1,33 +1,37 @@
 package com.earth.DownToEarth.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
+import javax.persistence.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@ToString
+@Entity
+@Table(name = "_users")
 public class User {
-    private Integer userId;
-    private String email;
-    private String username;
-    private String password;
-    private String firstname;
-    private String lastname;
-    private String location;  //optional
 
-    @Override
-    public String toString() {
-        return "\n" + "User{" +
-                "userId=" + userId +
-                ", email='" + email + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                ", firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", location='" + location + '\'' +
-                '}';
-    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = true)
+    private String location;  //optional
 }
