ef8cecc51b7a51a81593071489ce1fafe8868187
Trebolf <100227191+Trebolf@users.noreply.github.com>
2022-04-06 13:38:53 -0500

    Update PostController.java

--- DownToEarth/src/main/java/com/earth/DownToEarth/controllers/PostController.java
+++ DownToEarth/src/main/java/com/earth/DownToEarth/controllers/PostController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequestMapping("post")
 public class PostController {
 
-    @Autowired
+    @Autowired //field injection
     private PostService postService;
 
     @PostMapping
